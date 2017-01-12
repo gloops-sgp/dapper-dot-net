dc1c942f71a86d09a51e6f231d13b9396cdb16ec
s.yamada <s.yamada@gloops.com>
2017-01-12 11:27:56 +0900

    MySQLで使用した時のバグ（http://neue.cc/2013/08/06_423.html）に対応した

--- Dapper/SqlMapper.cs
+++ Dapper/SqlMapper.cs
@@ -971,8 +971,8 @@ namespace Dapper
             {
                 if (reader != null)
                 {
-                    if (!reader.IsClosed) try { cmd.Cancel(); }
-                        catch { /* don't spoil the existing exception */ }
+                    //if (!reader.IsClosed) try { cmd.Cancel(); }
+                    //    catch { /* don't spoil the existing exception */ }
                     reader.Dispose();
                 }
                 if (wasClosed) cnn.Close();
